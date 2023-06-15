2b09b0f9dad4c70ad113a780fc2211a6cba83b99
cadowlee <alimiabdullahi93@gmail.com>
2023-06-15 14:17:26 +0100

    loader clean up

--- src/components/pageloader/loader.jsx
+++ src/components/pageloader/loader.jsx
@@ -6,18 +6,20 @@ export default function Pageloader({ setLoading }) {
     useEffect(() => {
         const timer = setTimeout(() => {
             // Animate the loader elements using gsap
-            gsap.to(".loader-logo", {
+            gsap.to([".loader-logo", ], {
                 scale: 0,
-                duration: 1, // Set the duration of the animation
+                duration: 1.7, // Set the duration of the animation
                 onComplete: () => {
                     // Animation completed, set loading state to false
                     setLoading(false);
                 },
+               
             });
             gsap.to(".blinder", {
                 scaleY: 0,
                 stagger: 0.3,
-                duration: 1, // Set the duration of the animation
+                duration: .5, // Set the duration of the animation
+              
             });
         }, 5000);
 
@@ -27,11 +29,11 @@ export default function Pageloader({ setLoading }) {
     return (
         <div className="loader">
             <div className="blinder-container">
-                <div className="blinder"></div>
-                <div className="blinder"></div>
-                <div className="blinder"></div>
-                <div className="blinder"></div>
-                <div className="blinder"></div>
+                <div className="blinder blinder-1"></div>
+                <div className="blinder blinder-2"></div>
+                <div className="blinder blinder-3"></div>
+                <div className="blinder blinder-4"></div>
+                <div className="blinder blinder-5"></div>
             </div>
             <div className="loader-logo">
                 <h1>
